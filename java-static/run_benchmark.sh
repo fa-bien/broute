7f1ba4bfddf79abe6c29cc5b99fd8f188f50a88e
Fabien Tricoire (WU) <fabien.tricoire@wu.ac.at>
2020-09-08 18:35:53 +0200

    Or-opt: java implementation

--- java-static/run_benchmark.sh
+++ java-static/run_benchmark.sh
@@ -4,4 +4,4 @@ dirname=`basename \`pwd\``
 
 version=`java -version 2>&1 | head -n 1`
 
-java TSPBenchmark $1 | sed -e "s/java/$dirname,$version/g"
+java TSPBenchmark $1 $2 | sed -e "s/java/$dirname,$version/g"
