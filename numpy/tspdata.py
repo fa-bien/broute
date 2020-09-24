7eb27de5a714875b8d33dc41ba80f69aaf1c4142
Fabien Tricoire (WU) <fabien.tricoire@wu.ac.at>
2020-09-24 14:47:36 +0200

    less things are written twice

--- numpy/tspdata.py
+++ numpy/tspdata.py
@@ -5,47 +5,13 @@ import math
 
 import numpy
 
+import basedata
+
 mapsize = 100
 
 # encapsulate TSP data: instance + starting solutions to optimise for
 # benchmarking purpose
-class TSPData:
+class TSPData(basedata.TSPData):
     def __init__(self, n, d):
         self.n, self.d = n, numpy.array(d)
-        
-    def dumpstring(self):
-        out  = '# n n_solutions\n'
-        out += str(self.n) + ' ' + str(len(self.solutions)) + '\n'
-        out += '# n x n distance matrix\n'
-        for row in self.d:
-            out += ' '.join(str(x) for x in row) + '\n'
-        return out
-                    
-    def savetofile(self, fname):
-        with open(fname, 'w') as f:
-            f.write(self.dumpstring())
-            
-    def genrandom(self, n):
-        x = [ random.choice(range(mapsize)) for x in range(n) ]
-        y = [ random.choice(range(mapsize)) for x in range(n) ]
-        self.n = n
-        # Euclidean distances
-        self.d = [ [ int(math.hypot(x[i]-x[j], y[i]-y[j]) * 100)
-                     for j in range(n) ] for i in range(n) ]
-        # triangle inequality correction
-        for i in range(n):
-            for j in range(n):
-                if i == j: continue
-                for k in range(n):
-                    if i == k or j == k: continue
-                    if self.d[i][j] + self.d[j][k] < self.d[i][k]:
-                        self.d[i][k] = self.d[i][j] + self.d[j][k]
 
-
-    def genrandomsolution(self):
-        # solution generation: random permutations
-        # solutions always start and end with zero
-        points = [x for x in range(1, self.n)]
-        random.shuffle(points)
-        return [0] + points + [0]
-                        
