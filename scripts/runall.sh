fd205e5c2e0d224ea5fa1817aeb8662d0b541176
Fabien Tricoire (WU) <fabien.tricoire@wu.ac.at>
2020-10-22 14:48:43 +0200

    new more flexible script for running lal benchmarks Allows different set of implementations for each benchmark

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -6,38 +6,36 @@
 impldir=`pwd`'/implementations'
 instdir=`pwd`'/instances'
 
-# languages: list of command line arguments, or default list if no argument
-defaultlanguages='c++ c++-static-arrays c++98 julia rust java java-static-arrays numba javascript python pypy'
-languages="$@"
-if [[ "$#" -lt 1 ]]; then
-    languages=$defaultlanguages
-fi
-
-benchmarks='2-opt Or-opt lns'
-
 # we do multi-threading!
 # Number of threads = environment variable THREADS or default value
 [[ -z "$THREADS" ]] && THREADS=3
 
-for lang in $languages
-do
-    (echo "Running $lang"
-     (pushd $impldir'/'$lang > /dev/null
-      [[ -f compile.sh ]] && ./compile.sh
-      echo "language,version,benchmark,instance,n,nsolutions,checksum,time"
-      for bench in $benchmarks
-      do
-	  ./run_benchmark.sh $instdir $bench
-      done
-      popd > /dev/null
-     ) > $lang-runs.csv
-     echo "Done running $lang"
-    ) &
-    # parallelisation stolen from
-    #https://unix.stackexchange.com/questions/103920/parallelize-a-bash-for-loop
-    if [[ $(jobs -r -p | wc -l) -ge $THREADS ]]; then
-        wait -n
-    fi
+all_langs="c++ c++98 c++-hybrid-matrix c++-nested-matrix c++-static-arrays java java-nested-matrix javascript javascript-nested-matrix java-static-arrays julia-array julia-flat-matrix julia-square-matrix numba numba-flat-matrix numpy numpy-flat-matrix pypy pypy-nested-matrix python python-flat-matrix python-nested-matrix-function rust"
+interesting_langs="c++ c++98 java javascript julia-flat-matrix julia-square-matrix numba pypy python rust"
+#keys='moo woof'
+declare -A benchmarks=( ["2-opt"]=$all_langs
+			["Or-opt"]=$all_langs
+			["lns"]=$interesting_langs
+			# ["espprc"]=$interesting_langs
+		      )
+
+for benchmark in "${!benchmarks[@]}"; do
+    for lang in ${benchmarks[$benchmark]}; do
+	(echo -e "Running \e[1m$lang\e[0m implementation of \e[1m$benchmark\e[0m benchmark"
+	 (pushd $impldir'/'$lang > /dev/null
+	  [[ -f compile.sh ]] && ./compile.sh
+	  echo "language,version,benchmark,instance,n,nsolutions,checksum,time"
+	  ./run_benchmark.sh $instdir $benchmark
+	  popd > /dev/null
+	 ) > $benchmark-$lang-runs.csv
+	 echo "Done running $lang implementation of $benchmark"
+	) &
+	# parallelisation stolen from
+	#https://unix.stackexchange.com/questions/103920/parallelize-a-bash-for-loop
+	if [[ $(jobs -r -p | wc -l) -ge $THREADS ]]; then
+            wait -n
+	fi
+    done
 done
 
 wait
