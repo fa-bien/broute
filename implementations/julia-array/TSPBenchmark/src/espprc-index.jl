ff915dff374a0e1ff357ee7b267dcf0e99581979
fa-bien <f_tricoire@yahoo.fr>
2021-01-08 14:57:22 +0100

    - julia-array: code refactoring - julia array: espprc-index benchmark, same as espprc but uses a bit array of labels andindices in this array instead of references to labels

---
+++ implementations/julia-array/TSPBenchmark/src/espprc-index.jl
@@ -0,0 +1,140 @@
+using DataStructures
+
+mutable struct LabelWithIndex{ T <: Real }
+    at::Int
+    visits::Vector{Bool}
+    ignore::Bool
+    pred::Union{Nothing,Int}
+    cost::Float64
+    length::T
+    q::Vector{Int}
+    successors::Vector{Int}
+end
+
+function dominates(l1::T, l2::T) where T <: LabelWithIndex
+    if l1.cost > l2.cost || l1.length > l2.length
+        return false
+    end
+    for (v1, v2) ∈ zip(l1.visits, l2.visits)
+        if v1 > v2
+            return false
+        end
+    end
+    for (r1, r2) in zip(l1.q, l2.q)
+        if r1 > r2
+            return false
+        end
+    end
+    true
+end
+
+mutable struct LabelCollection{ T <: Real }
+    labels::Vector{LabelWithIndex{T}}
+end
+
+# create empty label
+function LabelWithIndex(d::MT, nresources::Int) where MT <: TSPData
+    LabelWithIndex(1, zeros(Bool, d.n), false, nothing, 0.0, 0,
+                   zeros(Int, nresources), zeros(Int, 0))
+end
+
+# create empty label and add it to lc
+function emptylabel(d::DT, lc::LCT, nresources::Int) where DT <: TSPData where LCT <: LabelCollection
+    push!(lc.labels, LabelWithIndex(d, nresources))
+    return length(lc.labels)
+end
+
+# extend label and add it to LC
+function extendlabel(d::MT, lc::LC, from::Int, to::Int) where MT <: TSPData where LC <: LabelCollection
+    visits = copy(lc.labels[from].visits)
+    visits[to] = true
+    q = [ (((to-1) & 1 << (i-1)) > 0) ? r + 1 : r
+          for (i,r) in enumerate(lc.labels[from].q) ]
+    nl = LabelWithIndex(to, visits, false, from,
+                        lc.labels[from].cost + d.aux[lc.labels[from].at, to],
+                        lc.labels[from].length + d.d[lc.labels[from].at, to], q,
+                        zeros(Int, 0))
+    push!(lc.labels, nl)
+    return length(lc.labels)
+end
+
+function marksuccessors!(lc::LC, index::Int) where LC <: LabelCollection
+    let l = lc.labels[index]
+        for s ∈ l.successors
+            lc.labels[s].ignore = true
+            marksuccessors!(lc, s)
+        end
+    end
+end
+
+# update given list of labels with new label
+# returns true if newlabel is added, false otherwise
+function updatedominance!(lc::LCT, labels::Vector{Int}, newlabel::Int) where LCT <: LabelCollection
+    i = 1
+    while i ≤ length(labels)
+        if dominates(lc.labels[labels[i]], lc.labels[newlabel])
+            return false
+        end
+        if dominates(lc.labels[newlabel], lc.labels[labels[i]])
+            marksuccessors!(lc, labels[i])
+            if i < length(labels)
+                labels[i] = labels[end]
+            end
+            pop!(labels)
+        else
+            i += 1
+        end
+    end
+    # at this point we know that newlabel is not dominated, so we add it
+    push!(labels, newlabel)
+    true
+end
+
+function solvewithindex(e::ESPPRC{MT, T}) where MT <: TSPData where T <: Real
+    # we store all labels here
+    lc = LabelCollection(LabelWithIndex{T}[])
+    # initial label
+    l = emptylabel(e.d, lc, e.nresources)
+    Q = Deque{Int}()
+    inQ = zeros(Bool, e.d.n)
+    push!(Q, 1)
+    inQ[1] = true
+    labels = [ Vector{Int}() for i in 1:e.d.n ]
+    push!(labels[1], l)
+    # main DP loop
+    while ! isempty(Q)
+        n = popfirst!(Q)
+        inQ[n] = false
+        for lindex ∈ labels[n]
+            lc.labels[lindex].ignore && continue
+            for succ ∈ 1:e.d.n
+                (lc.labels[lindex].visits[succ] || succ == n) && continue
+                # is it length-feasible?
+                if lc.labels[lindex].length + e.d.d[n, succ] + e.d.d[succ, 1] >
+                    e.maxlen
+                    continue
+                end
+                # is it resource-feasible?
+                rfeas = true
+                for (r, q) ∈ enumerate(lc.labels[lindex].q)
+                    if ((succ-1) & (1 << (r-1))) > 0 &&
+                        q + 1 > e.resourcecapacity
+                        rfeas = false
+                        break
+                    end
+                end
+                rfeas || continue
+                # at this point we know the extension is feasible so we do it
+                nl = extendlabel(e.d, lc, lindex, succ)
+                added = updatedominance!(lc, labels[succ], nl)
+                added && push!(lc.labels[lindex].successors, nl)
+                if added && (! inQ[succ]) && succ ≠ 1
+                    push!(Q, succ)
+                    inQ[succ] = true
+                end
+            end
+            lc.labels[lindex].ignore = true
+        end
+    end
+    Int(trunc(minimum(lc.labels[lindex].cost for lindex in labels[1])))
+end
---
+++ implementations/julia-array/TSPBenchmark/src/tsp.jl
@@ -0,0 +1,164 @@
+struct TSPData{ T<:Real }
+    n::Int    # number of nodes
+    d::Matrix{T} # distance matrix
+    aux::Matrix{Float64}  # auxiliary graph used e.g. in column generation
+end
+
+include("espprc.jl")
+include("espprc-index.jl")
+
+mutable struct TSPSolution
+    tour::Vector{Int}
+end
+
+function read_data(fname::String, T::DataType=Int)
+    lines = open(fname) do f
+        collect(eachline(f))
+    end
+    n = 0
+    drow = 1
+    d = zeros(T, 1, 1)
+    nsols = 0
+    solutions = TSPSolution[]
+    for line in lines
+        line = replace(line, r"#.*" => "")
+        tokens = split(line)
+        if length(tokens) == 0
+            continue
+        elseif length(tokens) == 2 && n == 0
+            n = parse(Int, tokens[1])
+            nsols = parse(Int, tokens[2])
+            d = zeros(T, n, n)
+        elseif drow ≤ n
+            row = [parse(T, x) for x in tokens]
+            d[drow,:] = row
+            drow += 1
+        elseif length(solutions) ≤ nsols
+            push!(solutions, TSPSolution([1+parse(Int, x) for x in tokens]))
+        end
+    end
+    aux = zeros(Float64, size(d))
+    data = TSPData{T}(n, d, aux)
+    return data, solutions
+end
+
+function two_opt(data::TSPData{T}, sol::TSPSolution) where T
+    t = 0
+    while first2eimprovement(data, sol)
+        t += 1
+    end
+    return t
+end
+
+function first2eimprovement(d::TSPData{T}, sol::TSPSolution) where T
+    for p1 in 1:length(sol.tour)-3, p2 in p1+2:length(sol.tour)-1
+        if d.d[sol.tour[p1], sol.tour[p1+1]] +
+            d.d[sol.tour[p2], sol.tour[p2+1]] >
+            d.d[sol.tour[p1], sol.tour[p2]] +
+            d.d[sol.tour[p1+1], sol.tour[p2+1]]
+            # improving 2-exchange found
+            for i in 0:(p2-p1+1) ÷ 2 - 1
+                sol.tour[p1+1+i], sol.tour[p2-i] =
+                    sol.tour[p2-i], sol.tour[p1+1+i]
+            end
+            return true
+        end
+    end
+    return false
+end
+
+function or_opt(data::TSPData{T}, sol::TSPSolution) where T
+    t = 0
+    while firstorimprovement(data, sol)
+        t += 1
+    end
+    return t
+end
+
+function firstorimprovement(d::TSPData{T}, s::TSPSolution) where T
+    for i in 2:length(s.tour)-1, l in 1:(min(3, length(s.tour)-i)),
+        p in [ 1:i-2 ; i+l:length(s.tour)-1 ]
+        #
+        δ = d.d[s.tour[i-1], s.tour[i+l]] + d.d[s.tour[p], s.tour[i]] +
+            d.d[s.tour[i+l-1], s.tour[p+1]] -
+            d.d[s.tour[p], s.tour[p+1]] - d.d[s.tour[i-1], s.tour[i]] -
+            d.d[s.tour[i+l-1], s.tour[i+l]]
+        # improvement found
+        if δ < 0
+            seq = [ k for k in s.tour[i:i+l-1] ]
+            if i < p
+                for j in i:p-l
+                    s.tour[j] = s.tour[j+l]
+                end
+                for j in 1:l
+                    s.tour[p+j-l] = seq[j]
+                end
+            else
+                for j in i-1:-1:p+1
+                    s.tour[j+l] = s.tour[j]
+                end
+                for j in 1:l
+                    s.tour[p+j] = seq[j]
+                end
+            end
+            return true
+        end
+    end
+    return false
+end
+
+function lns(d::TSPData{T}, sol::TSPSolution, niter::Int=10) where T
+    checksum = zero(T)
+    for iter in 1:niter
+        # step 0: copy incumbent
+        tmp = copy(sol.tour)
+        unplanned = Int[]
+        # step 1: destroy
+        t = 2
+        while t < length(tmp)
+            push!(unplanned, tmp[t])
+            deleteat!(tmp, t)
+            t += 1
+        end
+        # step 2: repair
+        while length(unplanned) > 0
+            bestcost, bestfrom, bestto = typemax(T), 0, 0
+            for from in 1:length(unplanned), to in 1:length(tmp)-1
+                δ = d.d[tmp[to], unplanned[from]] +
+                    d.d[unplanned[from], tmp[to+1]] -
+                    d.d[tmp[to], tmp[to+1]]
+                if δ < bestcost
+                    bestcost, bestfrom, bestto = δ, from, to
+                end
+            end
+            # perform best found insertion
+            insert!(tmp, bestto + 1, unplanned[bestfrom])
+            deleteat!(unplanned, bestfrom)
+            checksum += bestcost
+        end
+        # step 3: move to new incumbent
+        sol.tour = tmp
+    end
+    checksum
+end
+
+function espprc(d::T, sol::ST;
+                nresources::Int=6, resourcecapacity::Int=1,
+                index::Bool=false) where T <: TSPData where ST <: TSPSolution
+    dual = zeros(Float64, d.n)
+    for (i, j) in zip(sol.tour[1:end-1], sol.tour[2:end])
+        dual[j] = d.d[i, j]
+    end
+    for i ∈ 1:d.n, j ∈ 1:d.n
+        d.aux[i, j] = d.d[i, j] - dual[j]
+    end
+    # max len: sum of best assignments
+    maxlen = sum( [ minimum([d.d[i, j] for j ∈ 1:d.n if i ≠ j])
+                    for i ∈ 1:d.n ] )
+    e = ESPPRC(d, nresources, resourcecapacity, maxlen)
+    if index
+        solvewithindex(e)
+    else
+        solve(e)
+    end
+end
