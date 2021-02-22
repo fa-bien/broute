944e69593b31111bb57db411e83c061d503dcf41
fa-bien <f_tricoire@yahoo.fr>
2021-02-22 19:36:08 +0100

    bug fix in csv output

--- implementations/java/run_benchmark.sh
+++ implementations/java/run_benchmark.sh
@@ -4,4 +4,4 @@ dirname=`basename \`pwd\``
 
 version=`java -version 2>&1 | head -n 1`
 
-java TSPBenchmark $1 $2 | sed -e "s/java,/$dirname,/g" -e "s/,flat,/,flat,$version/g" -e "s/,nested,/,nested,$version/g"
+java TSPBenchmark $1 $2 | sed -e "s/java,/$dirname,/g" -e "s/,flat,/,flat,$version,/g" -e "s/,nested,/,nested,$version,/g"
