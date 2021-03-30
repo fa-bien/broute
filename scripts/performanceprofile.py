5607ed5bf00db85d3e2ce1eb17e47ff72df4fc82
fa-bien <f_tricoire@yahoo.fr>
2021-03-30 23:10:01 +0200

    performance profile generation!

---
+++ scripts/performanceprofile.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+import numbers
+
+import matplotlib.pyplot as plt
+
+def isNumber(x):
+    return isinstance(x, numbers.Number)
+
+lineStyles = [ 'solid',
+               (0, (8, 4)),
+               'dotted',
+               (0, (8, 4, 2, 2)),
+               (0, (8, 4, 2, 2, 2, 2, 2, 2)),
+               'dotted',
+               'dashdot'
+]
+lineColours = [ '#ae3638', '#3b69ac', 'green', 'orange', 'purple', 'brown',
+                'pink' ]
+
+# 'methods' is a list of methods to compare
+# 'instances' is a list of instances to use for that comparison
+# 'results' is a dict where results[i][m] is the performance (to be minimised)
+# of method m on instance i
+def plotPerformanceProfile(methods, instances, results,
+                           fName='perfprof.pdf', descriptions=None,
+                           title=None, logscale=False, interactive=True,
+                           verbose=False):
+    if not descriptions:
+        descriptions = methods
+    bestPerf = {}
+    for inst in instances:
+        allPerfs = [ results[inst][met] for met in methods
+                     if isNumber(results[inst][met]) ]
+        if len(allPerfs) > 0:
+            bestPerf[inst] = min( allPerfs )
+    perfRatios = {}
+    for met in methods:
+        perfRatios[met] = [ results[inst][met] / bestPerf[inst]
+                            for inst in instances
+                            if isNumber(results[inst][met]) ]
+    allSteps = {}
+    for met in methods:
+        sr = sorted(perfRatios[met])
+        steps = []
+        for a, (i, b) in zip(sr[:-1], enumerate(sr[1:])):
+            if a != b:
+                steps.append( (a, (i + 1.0) / len(bestPerf) ) )
+        steps.append( (sr[-1], float(len(sr)) / len(bestPerf)) )
+        allSteps[met] = steps
+    # compute bound
+    worstPerf = max( [ allSteps[met][-1][0] for met in methods ] )
+    #
+    if verbose:
+        print('worst performance:', worstPerf)
+        for met in methods:
+            for inst in instances:
+                if isNumber(results[inst][met]) and \
+                   results[inst][met] / bestPerf[inst] == worstPerf:
+                    print('\tmethod:', met, '\tinstance:', inst)
+    # cosmetics
+    fig = plt.figure(figsize=(10, 5))
+    ax = plt.subplot()
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    ax.tick_params(axis='both', which='major', labelsize=14)
+    ax.axis( [1, worstPerf + .005 * (worstPerf - 1), 0, 1.02] )
+    ax.set_yticks( [ .1 * x for x in range(11) ] )
+    ax.yaxis.grid(True, ls='dotted')
+    if logscale:
+        ax.set_xscale('log')
+    if title:
+        ax.set_title(title, fontsize=16)
+    # now we plot all of it
+    for (i, met), name in zip(enumerate(methods), descriptions):
+        xs = [ x[0] for x in allSteps[met] ]
+        ys = [ x[1] for x in allSteps[met] ]
+        xs.append(worstPerf)
+        if len(perfRatios[met]) == len(bestPerf):
+            ys.append(1)
+        else:
+            ys.append(ys[-1])
+        ax.plot( xs, ys, drawstyle='steps-post',
+                 label=name,
+                 linestyle=lineStyles[i],
+                 # antialiased=False,
+                 color=lineColours[i],
+                 linewidth=2)
+        leg = ax.legend(loc='lower right', borderaxespad=.5, fontsize=14,
+                        handlelength=3
+                        # numpoints=50
+        )
+        # leg = plt.legend()
+        leg.set_draggable(True)
+    if interactive:
+        plt.show()
+    fig.savefig(fName, bbox_inches='tight')
+    if verbose or interactive:
+        print('saved figure to', fName)
+
+def showAllFigures():
+    plt.show()
---
+++ scripts/profiles.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+import performanceprofile as pp
+from functools import reduce
+
+def load_all_CSVs(dirname):
+    all_data = {}
+    instances = set()
+    read = 0
+    for fn in os.listdir(dirname):
+        if fn.endswith('.csv'):
+            read += 1
+            keys = []
+            with open(os.path.join(dirname, fn)) as f:
+                for line in f:
+                    if keys == []:
+                        keys = line.rstrip().split(',')
+                    else:
+                        tmp = {}
+                        for key, value in zip(keys, line.rstrip().split(',')):
+                            tmp[key] = value
+                        all_data[tmp['implementation'], tmp['language'],
+                                 tmp['matrix'], tmp['benchmark'],
+                                 tmp['instance']] = float(tmp['time'])
+                        instances.add(tmp['instance'])
+    return instances, all_data
+
+def normalise_time(data):
+    for key in data:
+        impl, lang, mat, bench, inst = key
+        if impl != 'c++14':
+            data[key] /= data['c++14', 'C++14', 'flat', bench, inst]
+    for key in data:
+        impl, lang, mat, bench, inst = key
+        if impl == 'c++14':
+            data[key] = 1.0
+
+def makeperfprofs(instances, data, benchmarks, comps,
+                  titleprefix='Performance', fileprefix=''):
+    impls = reduce(lambda x, y: x+y, (x[1] for x in comps))
+    results = { bench: {i:{} for i in instances } for bench in benchmarks }
+    for key in data:
+        impl, lang, mat, bench, inst = key
+        if impl in impls and bench in benchmarks:
+            results[bench][inst][impl] = data[key]
+    for bench in benchmarks:
+        for lang, methods, names in comps:
+            title = titleprefix + ' (' + (lang + ', ' if lang != '' else '') + bench + ')'
+            fName = fileprefix + lang + '-' + bench + '-perfprof.pdf'
+            pp.plotPerformanceProfile(methods, instances, results[bench],
+                                      title=title,
+                                      fName=fName,
+                                      descriptions=names)
+
+def make_profiles(instances, data):
+    
+    # # Impact of using a flat matrix
+    # benchmarks = ('2-opt', 'Or-opt')
+    # comps = [ ('C++14', ('c++14', 'c++14-nested-matrix'), ('Flat', 'Nested')),
+    #           ('Java', ('java', 'java-nested-matrix'), ('Flat', 'Nested')),
+    #           ('JavaScript', ('javascript', 'javascript-nested-matrix'),
+    #            ('Flat', 'Nested')),
+    #           ('Julia', ('julia-flat-matrix', 'julia-array'),
+    #            ('Flat', 'Square')),
+    #           ('Python', ('python-flat-matrix', 'python'), ('Flat', 'Nested')),
+    #           ('Pypy', ('pypy', 'pypy-nested-matrix'), ('Flat', 'Nested')),
+    #           ('Numba', ('numba-flat-matrix', 'numba'), ('Flat', 'Square')),
+    #           ('Numpy', ('numpy-flat-matrix', 'numpy'), ('Flat', 'Square')),
+    #          ]
+    # titlep = 'CPU performance of matrix representation'
+    # filep = 'matrix-'
+    # makeperfprofs(instances, data, benchmarks, comps,
+    #               titleprefix=titlep, fileprefix=filep)
+    
+    # # Impact of using static arrays
+    # comps = [ ('C++14', ('c++14', 'c++14-static-arrays'),('Dynamic', 'Static')),
+    #           ('Java', ('java', 'java-static-arrays'), ('Dynamic', 'Static')) ]
+    # titlep = 'CPU performance of array data structure'
+    # filep = 'array-'
+    # makeperfprofs(instances, data, benchmarks, comps,
+    #               titleprefix=titlep, fileprefix=filep)
+    
+    # ## Comparison of Python implementation: performance of Python interpreters
+    # # we proceed in two steps since Numba only implements 2 benchmarks
+    # benchmarks = ('2-opt', 'Or-opt')
+    # comps = [ ('', ('python', 'pypy', 'numpy', 'numba'),
+    #            ('CPython', 'Pypy', 'Numpy', 'Numba')) ]
+    # titlep = 'CPU performance of Python interpreters'
+    # filep = 'python_interpreters'
+    # makeperfprofs(instances, data, benchmarks, comps,
+    #               titleprefix=titlep, fileprefix=filep)
+    # # Zoom in on Pypy vs Numba
+    # comps = [ ('', ('pypy', 'numba'), ('Pypy', 'Numba')) ]
+    # titlep = 'Pypy vs Numba'
+    # filep = 'pypy_vs_numba'
+    # makeperfprofs(instances, data, benchmarks, comps,
+    #               titleprefix=titlep, fileprefix=filep)
+    
+    # ## A comparison of C++ implementations
+    # benchmarks = ('2-opt', 'Or-opt', 'lns', 'espprc', 'maxflow')
+    # comps = [ ('', ('c++14', 'c++98'), ('C++14', 'C++98')) ]
+    # titlep = 'C++14 vs C++98'
+    # filep = 'C++14_vs_C++98-'
+    # makeperfprofs(instances, data, benchmarks, comps,
+    #               titleprefix=titlep, fileprefix=filep)
+    
+    # ## General cross-language comparison...
+    # benchmarks = ('2-opt', 'Or-opt', 'lns', 'maxflow', 'espprc-index')
+    # comps = [ ('', ('c++98', 'java', 'javascript', 'julia-flat-matrix',
+    #                 'pypy', 'rust'),
+    #            ('C++98', 'Java', 'JavaScript', 'Julia', 'Python', 'Rust')) ]
+    # titlep = 'General cross-language comparison'
+    # filep = 'cross_language'
+    # makeperfprofs(instances, data, benchmarks, comps,
+    #               titleprefix=titlep, fileprefix=filep)
+    # ## ... including espprc without Rust
+    # benchmarks = ('espprc',)
+    # comps = [ ('', ('c++98', 'java', 'javascript', 'julia-flat-matrix', 'pypy'),
+    #            ('C++98', 'Java', 'JavaScript', 'Julia', 'Python')) ]
+    # titlep = 'General cross-language comparison'
+    # filep = 'cross_language'
+    # makeperfprofs(instances, data, benchmarks, comps,
+    #               titleprefix=titlep, fileprefix=filep)
+
+    # ## Cross-language comparison: fast languages
+    # benchmarks = ('2-opt', 'Or-opt', 'lns', 'maxflow', 'espprc-index')
+    # comps = [ ('', ('c++98', 'java', 'julia-flat-matrix', 'rust'),
+    #            ('C++98', 'Java', 'Julia', 'Rust')) ]
+    # titlep = 'Cross-language comparison: "Fast" languages'
+    # filep = 'fast_languages'
+    # makeperfprofs(instances, data, benchmarks, comps,
+    #               titleprefix=titlep, fileprefix=filep)
+    # ## ... including espprc without Rust
+    # benchmarks = ('espprc',)
+    # comps = [ ('', ('c++98', 'java', 'julia-flat-matrix'),
+    #            ('C++98', 'Java', 'Julia')) ]
+    # titlep = 'Cross-language comparison: "Fast" languages'
+    # filep = 'fast_languages'
+    # makeperfprofs(instances, data, benchmarks, comps,
+    #               titleprefix=titlep, fileprefix=filep)
+    
+    ## Cross-language comparison: interpreted languages
+    benchmarks = ('2-opt', 'Or-opt', 'lns', 'maxflow', 'espprc')
+    comps = [ ('', ('pypy', 'javascript'),
+               ('Python', 'JavaScript')) ]
+    titlep = 'Cross-language comparison: "Interpreted" languages'
+    filep = 'interpreted_languages'
+    makeperfprofs(instances, data, benchmarks, comps,
+                  titleprefix=titlep, fileprefix=filep)
+    
+    ## Cross-platform comparison
+    # Not yet clear what to do here!
+    
+def main():
+    csv_dir = '.'
+    if len(sys.argv) > 1:
+        csv_dir = sys.argv[1]
+    instances, data = load_all_CSVs(csv_dir)
+    # normalise_time(data)
+    make_profiles(instances, data)
+
+if __name__ == '__main__':
+    main()
