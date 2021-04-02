41657ab7cd20fe698107910f635ba9225316b38f
fa-bien <f_tricoire@yahoo.fr>
2021-04-02 09:55:05 +0200

    near-final version of scripts generating all comparison figures

--- scripts/compare.R
+++ scripts/compare.R
@@ -39,170 +39,193 @@ runs$normalised <- runs$time / runs$reftime
 
 ## Now we can generate charts
 
+## distance matrix representation comparison
+ds <- subset(runs,
+             implementation %in% c('c++14', 'c++14-nested-matrix',
+                                   'java', 'java-nested-matrix',
+                                   'javascript', 'javascript-nested-matrix',
+                                   'julia-flat-matrix', 'julia-array'))
+for (lang in c('C++14', 'Java', 'JavaScript', 'Julia')) {
+    for (bench in c('2-opt', 'Or-opt')) {
+        pdf(paste0('matrix-', lang, '-', bench, '-boxplot.pdf'))
+        title <- paste0(lang, ': relative CPU effort of matrix representations (', bench, ')')
+        p <- ggplot(subset(ds, language == lang & benchmark == bench),
+                    aes(x=factor(n), y=normalised, fill=matrix)) +
+            geom_boxplot() + labs(x='n',
+                                  y='CPU time (ratio of C++14 time)',
+                                  fill='Matrix representation',
+                                  title=title) + theme_bw()
+        print(p)
+        dev.off()
+    }
+}
+
+## FAILED EXPERIMENT might work well with less outliers but as things are now,
+##                   a bit hard to read
 ## ## distance matrix representation comparison
 ## ds <- subset(runs,
 ##              implementation %in% c('c++14', 'c++14-nested-matrix',
 ##                                    'java', 'java-nested-matrix',
 ##                                    'javascript', 'javascript-nested-matrix',
-##                                    'julia-flat-matrix', 'julia-array',
-##                                    'python', 'python-flat-matrix'))
-## for (lang in c('C++14', 'Java', 'JavaScript', 'Julia', 'Python')) {
-##     for (bench in c('2-opt', 'Or-opt')) {
-##         pdf(paste0('matrix-', lang, '-', bench, '-boxplot.pdf'))
-##         title <- paste0('Relative CPU effort depending on matrix representation\n(', lang, ', ', bench, ')')
-##         p <- ggplot(subset(ds, language == lang & benchmark == bench),
-##                     aes(x=factor(n), y=normalised, fill=matrix)) +
-##             geom_boxplot() + labs(x='n',
-##                                   y='CPU time (ratio of C++14 time)',
-##                                   fill='Matrix representation',
-##                                   title=title)
-##         print(p)
-##         dev.off()
-##     }
-## }
-
-## ## Impact of using static arrays
-## ds <- subset(runs, implementation %in% c('c++14', 'c++14-static-arrays',
-##                                          'java', 'java-static-arrays'))
-## for (lang in c('C++14', 'Java')) {
-##     for (bench in c('2-opt', 'Or-opt')) {
-##         pdf(paste0('array-', lang, '-', bench, '-boxplot.pdf'))
-##         title <- paste0('Relative CPU effort depending on array type\n(',
-##                         bench, ')')
-##         p <- ggplot(subset(ds, language == lang & benchmark == bench),
-##                     aes(x=factor(n), y=normalised, fill=implementation)) +
-##             geom_boxplot() + labs(x='n',
-##                                   y='CPU time (ratio of C++14 time)',
-##                                   fill='Implementation',
-##                                   title=title)
-##         print(p)
-##         dev.off()
-##     }
-## }
-
-## ## A comparison of Python implementations
-## ## Flat vs Nested matrix depending on Python interpreter
-## ds <- subset(runs, language == 'Python')
-## ds$impl <- as.character(ds$implementation)
-## ds <- ds %>% mutate(interpreter = case_when(
-##                         startsWith(impl, 'pypy') ~ 'Pypy',
-##                         startsWith(impl, 'numba') ~ 'Numba',
-##                         startsWith(impl, 'numpy') ~ 'Numpy',
-##                         TRUE ~ 'CPython'))
-
+##                                    'julia-flat-matrix', 'julia-array'))
 ## for (bench in c('2-opt', 'Or-opt')) {
-##     for (inter in c('CPython', 'Pypy', 'Numba', 'Numpy')) {
-##         pdf(paste0('matrix-', inter, '-', bench, '-boxplot.pdf'))
-##         title <- paste0(inter,
-##                         ': relative CPU effort depending on array type (',
-##                         bench, ')')
-##         p <- ggplot(subset(ds, interpreter == inter & benchmark == bench),
-##                     aes(x=factor(n), y=normalised, fill=matrix)) +
-##             geom_boxplot() + labs(x='n',
-##                                   y='CPU time (ratio of C++14 time)',
-##                                   fill='Implementation',
-##                                   title=title)
-##         print(p)
-##         dev.off()
-##     }
-## }
-
-## ## performance of Python interpreters
-## pythonbest <- subset(ds,
-##                      implementation %in% c('python', 'pypy', 'numpy', 'numba'))
-## for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'maxflow')) {
-##     title <- paste0('Relative CPU effort of Python interpreters (', bench, ')')
-##     pdf(paste0('python_interpreters-', bench, '-boxplot.pdf'))
-##     p <- ggplot(subset(pythonbest, benchmark == bench),
-##                 aes(x=factor(n), y=normalised,
-##                     fill=factor(interpreter,
-##                                 levels=c('CPython','Pypy','Numpy','Numba')))) +
-##         geom_boxplot() + labs(x='n',
-##                               y='CPU time (ratio of C++14 time)',
-##                               fill='Interpreter',
-##                               title=title)
-##     print(p)
-##     dev.off()
-## }
-## for (bench in c('2-opt', 'Or-opt')) {
-##     title <- paste0('Relative CPU effort of Pypy vs Numba (', bench, ')')
-##     pdf(paste0('pypy_vs_numba-', bench, '-boxplot.pdf'))
-##     p <- ggplot(subset(pythonbest, benchmark == bench &
-##                        interpreter %in% c('Pypy', 'Numba')),
-##                 aes(x=factor(n), y=normalised,
-##                     fill=factor(interpreter,))) +
-##         geom_boxplot() + labs(x='n',
-##                               y='CPU time (ratio of C++14 time)',
-##                               fill='Interpreter',
-##                               title=title)
-##     print(p)
-##     dev.off()
-## }
-
-## ## A comparison of C++ implementations
-## ds <- subset(runs, implementation %in% c('c++14', 'c++98'))
-## for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'maxflow')) {
-##     pdf(paste0('C++14_vs_C++98-', bench, '-boxplot.pdf'))
-##     title <- paste0('C++14 vs C++98 (', bench, ')')
+##     pdf(paste0('matrix-', bench, '-boxplot.pdf'))
+##     title <- paste0('Relative CPU effort with matrix representation (', bench, ')')
 ##     p <- ggplot(subset(ds, benchmark == bench),
-##                 aes(x=factor(n), y=normalised, fill=language)) +
+##                 aes(x=factor(n), y=normalised, fill=matrix)) +
+##         facet_wrap(~language, scales='free_y') +
 ##         geom_boxplot() + labs(x='n',
 ##                               y='CPU time (ratio of C++14 time)',
-##                               fill='Language',
+##                               fill='Matrix representation',
 ##                               title=title)
 ##     print(p)
 ##     dev.off()
 ## }
 
-## ## General cross-language comparison
-## ds <- subset(runs, implementation %in%
-##                    c('c++98', 'java', 'javascript', 'julia-flat-matrix',
-##                      'pypy', 'rust'))
-## for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'espprc-index', 'maxflow')){
-##     pdf(paste0('cross_language-', bench, '-boxplot.pdf'))
-##     title <- paste0('General cross-language comparison (', bench, ')')
-##     p <- ggplot(subset(ds, benchmark == bench),
-##                 aes(x=factor(n), y=normalised, fill=language)) +
-##         geom_boxplot() + labs(x='n',
-##                               y='CPU time (ratio of C++14 time)',
-##                               fill='Language',
-##                               title=title)
-##     print(p)
-##     dev.off()
-## }
+## Impact of using static arrays
+ds <- subset(runs, implementation %in% c('c++14', 'c++14-static-arrays',
+                                         'java', 'java-static-arrays'))
+for (lang in c('C++14', 'Java')) {
+    for (bench in c('2-opt', 'Or-opt')) {
+        pdf(paste0('array-', lang, '-', bench, '-boxplot.pdf'))
+        title <- paste0(lang, ': relative CPU effort of array type\n(',
+                        bench, ')')
+        p <- ggplot(subset(ds, language == lang & benchmark == bench),
+                    aes(x=factor(n), y=normalised, fill=implementation)) +
+            geom_boxplot() + labs(x='n',
+                                  y='CPU time (ratio of C++14 time)',
+                                  fill='Implementation',
+                                  title=title) + theme_bw()
+        print(p)
+        dev.off()
+    }
+}
 
-## ## Cross-language comparison: fast languages
-## ds <- subset(runs, implementation %in%
-##                    c('c++98', 'java', 'julia-flat-matrix', 'rust'))
-## for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'espprc-index', 'maxflow')){
-##     pdf(paste0('fast_languages-', bench, '-boxplot.pdf'))
-##     title <- paste0('General comparison of "fast" languages (', bench, ')')
-##     p <- ggplot(subset(ds, benchmark == bench),
-##                 aes(x=factor(n), y=normalised, fill=language)) +
-##         geom_boxplot() + labs(x='n',
-##                               y='CPU time (ratio of C++14 time)',
-##                               fill='Language',
-##                               title=title)
-##     print(p)
-##     dev.off()
-## }
+## A comparison of Python implementations
+## Flat vs Nested matrix depending on Python interpreter
+ds <- subset(runs, implementation %in% c('python', 'python-flat-matrix',
+                                         'pypy', 'pypy-nested-matrix',
+                                         'numpy', 'numpy-flat-matrix',
+                                         'numba', 'numba-flat-matrix'))
+ds$impl <- as.character(ds$implementation)
+ds <- ds %>% mutate(interpreter = case_when(
+                        startsWith(impl, 'pypy') ~ 'Pypy',
+                        startsWith(impl, 'numba') ~ 'Numba',
+                        startsWith(impl, 'numpy') ~ 'Numpy',
+                        TRUE ~ 'CPython'))
 
-## ## Cross-language comparison: interpreted languages
-## ds <- subset(runs, implementation %in%
-##                    c('pypy', 'javascript'))
-## for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'maxflow')){
-##     pdf(paste0('interpreted_languages-', bench, '-boxplot.pdf'))
-##     title <- paste0('General comparison of "interpreted" languages (',
-##                     bench, ')')
-##     p <- ggplot(subset(ds, benchmark == bench),
-##                 aes(x=factor(n), y=normalised, fill=language)) +
-##         geom_boxplot() + labs(x='n',
-##                               y='CPU time (ratio of C++14 time)',
-##                               fill='Language',
-##                               title=title)
-##     print(p)
-##     dev.off()
-## }
+for (bench in c('2-opt', 'Or-opt')) {
+    for (inter in c('CPython', 'Pypy', 'Numba', 'Numpy')) {
+        pdf(paste0('matrix-', inter, '-', bench, '-boxplot.pdf'))
+        title <- paste0(inter,
+                        ': relative CPU effort of matrix representation (', bench, ')')
+        p <- ggplot(subset(ds, interpreter == inter & benchmark == bench),
+                    aes(x=factor(n), y=normalised, fill=matrix)) +
+            geom_boxplot() + labs(x='n',
+                                  y='CPU time (ratio of C++14 time)',
+                                  fill='Python interpreter',
+                                  title=title) + theme_bw()
+        print(p)
+        dev.off()
+    }
+}
+
+## performance of Python interpreters
+pythonbest <- subset(ds,
+                     implementation %in% c('python', 'pypy', 'numpy', 'numba'))
+for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'maxflow')) {
+    title <- paste0('Relative CPU effort of Python interpreters (', bench, ')')
+    pdf(paste0('python_interpreters-', bench, '-boxplot.pdf'))
+    p <- ggplot(subset(pythonbest, benchmark == bench),
+                aes(x=factor(n), y=normalised,
+                    fill=factor(interpreter,
+                                levels=c('CPython','Pypy','Numpy','Numba')))) +
+        geom_boxplot() + labs(x='n',
+                              y='CPU time (ratio of C++14 time)',
+                              fill='Interpreter',
+                              title=title) + theme_bw()
+    print(p)
+    dev.off()
+}
+for (bench in c('2-opt', 'Or-opt')) {
+    title <- paste0('Pypy vs Numba (', bench, ')')
+    pdf(paste0('pypy_vs_numba-', bench, '-boxplot.pdf'))
+    p <- ggplot(subset(pythonbest, benchmark == bench &
+                       interpreter %in% c('Pypy', 'Numba')),
+                aes(x=factor(n), y=normalised,
+                    fill=factor(interpreter,))) +
+        geom_boxplot() + labs(x='n',
+                              y='CPU time (ratio of C++14 time)',
+                              fill='Interpreter',
+                              title=title) + theme_bw()
+    print(p)
+    dev.off()
+}
+
+## A comparison of C++ implementations
+ds <- subset(runs, implementation %in% c('c++14', 'c++98'))
+for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'maxflow')) {
+    pdf(paste0('C++14_vs_C++98-', bench, '-boxplot.pdf'))
+    title <- paste0('C++14 vs C++98 (', bench, ')')
+    p <- ggplot(subset(ds, benchmark == bench),
+                aes(x=factor(n), y=normalised, fill=language)) +
+        geom_boxplot() + labs(x='n',
+                              y='CPU time (ratio of C++14 time)',
+                              fill='Language',
+                              title=title) + theme_bw()
+    print(p)
+    dev.off()
+}
+
+## General cross-language comparison
+ds <- subset(runs, implementation %in%
+                   c('c++98', 'java', 'javascript', 'julia-flat-matrix',
+                     'pypy', 'rust'))
+for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'espprc-index', 'maxflow')){
+    pdf(paste0('cross_language-', bench, '-boxplot.pdf'))
+    title <- paste0('General cross-language comparison (', bench, ')')
+    p <- ggplot(subset(ds, benchmark == bench),
+                aes(x=factor(n), y=normalised, fill=language)) +
+        geom_boxplot() + labs(x='n',
+                              y='CPU time (ratio of C++14 time)',
+                              fill='Language',
+                              title=title) + theme_bw()
+    print(p)
+    dev.off()
+}
+
+## Cross-language comparison: fast languages
+ds <- subset(runs, implementation %in%
+                   c('c++98', 'java', 'julia-flat-matrix', 'rust'))
+for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'espprc-index', 'maxflow')){
+    pdf(paste0('fast_languages-', bench, '-boxplot.pdf'))
+    title <- paste0('Relative performance of "fast" languages (', bench, ')')
+    p <- ggplot(subset(ds, benchmark == bench),
+                aes(x=factor(n), y=normalised, fill=language)) +
+        geom_boxplot() + labs(x='n',
+                              y='CPU time (ratio of C++14 time)',
+                              fill='Language',
+                              title=title) + theme_bw()
+    print(p)
+    dev.off()
+}
+
+## Cross-language comparison: interpreted languages
+ds <- subset(runs, implementation %in%
+                   c('pypy', 'javascript'))
+for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'maxflow')){
+    pdf(paste0('interpreted_languages-', bench, '-boxplot.pdf'))
+    title <- paste0('Relative performance of "interpreted" languages (',
+                    bench, ')')
+    p <- ggplot(subset(ds, benchmark == bench),
+                aes(x=factor(n), y=normalised, fill=language)) +
+        geom_boxplot() + labs(x='n',
+                              y='CPU time (ratio of C++14 time)',
+                              fill='Language',
+                              title=title) + theme_bw()
+    print(p)
+    dev.off()
+}
 
 ## Cross-platform comparison
 ## not clear what to do here - maybe just mention cases where different platforms yield different results?
