c526e7d2165c53408b11b6d2ea5bc45e31e25430
fa-bien <f_tricoire@yahoo.fr>
2021-04-02 09:50:03 +0200

    fixed old corrected version of running script

--- implementations/julia-array/run_benchmark.sh
+++ implementations/julia-array/run_benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#julia --check-bounds=no --inline=yes -O3 tspbench.jl $1
+#julia --check-bounds=no --inline=yes -O3 tspbench.jl $1 $2
 
 # only works from Julia 1.5 on
 julia --check-bounds=no --inline=yes -O3 -t 1 tspbench.jl $1 $2
