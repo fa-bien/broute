0a342d0824538f5fe096454f6b89cc29b3f8ce68
fa-bien <f_tricoire@yahoo.fr>
2021-12-01 12:46:39 +0100

    new look for the box plots: - different values of n are visually separated using facets - colour and fill are matching, fill has alpha blending, this way small flat boxes still provide colour info - colour-blind-friendly colours - removed some lines from the theme

--- scripts/compare.R
+++ scripts/compare.R
@@ -2,6 +2,18 @@
 
 library(dplyr)
 library(ggplot2)
+library(colorspace)
+
+colours <- c("#2571ac", "#cb6528", "#c37ea3", "#000000", "#f0ed60", "#3c9975")
+palette <- scale_color_manual(values=colours)
+
+lines <- element_line(size=.3)
+mytheme <- theme(axis.text.x=element_blank(),
+                 axis.ticks.x = element_blank(),
+                 panel.grid.major.x = element_blank(),
+                 panel.grid.minor.x = element_blank(),
+                 panel.grid.major.y = lines,
+                 panel.grid.minor.y = lines)
 
 ## reference implementation for ratio calculation
 refimpl <- 'c++14'
@@ -24,7 +36,7 @@ for (col in c('implementation', 'language', 'matrix', 'version',
     all_runs[,col] <- factor(all_runs[,col])
 }
 
-## we isolate CPU times of referenec implementation
+## we isolate CPU times of reference implementation
 refdata <- filter(all_runs, implementation==refimpl)
 refdata$reftime <- refdata$time
 normalised <- left_join(all_runs, refdata,
@@ -53,37 +65,16 @@ for (lang in c('C++14', 'Java', 'JavaScript', 'Julia')) {
         pdf(paste0('matrix-', lang, '-', bench, '-boxplot.pdf'))
         title <- paste0(lang, ': relative CPU effort of matrix representations (', bench, ')')
         p <- ggplot(subset(ds, language == lang & benchmark == bench),
-                    aes(x=factor(n), y=normalised, fill=matrix)) +
-            geom_boxplot() + labs(x='n',
-                                  y='CPU time (ratio of C++14 time)',
-                                  fill='Matrix representation',
-                                  title=title) + theme_bw()
+                    aes(y=normalised, colour=matrix, fill=after_scale(color))) +
+            facet_wrap(~n, strip.position="bottom") + 
+            geom_boxplot(alpha=0.2, outlier.alpha=.5) +
+            labs(x='n', y='CPU time (ratio of C++14 time)',
+                 title=title, color='Matrix\nrepresentation') +
+            theme_bw() + mytheme + palette
         print(p)
         dev.off()
     }
 }
-
-## FAILED EXPERIMENT might work well with less outliers but as things are now,
-##                   a bit hard to read
-## ## distance matrix representation comparison
-## ds <- subset(runs,
-##              implementation %in% c('c++14', 'c++14-nested-matrix',
-##                                    'java', 'java-nested-matrix',
-##                                    'javascript', 'javascript-nested-matrix',
-##                                    'julia-flat-matrix', 'julia-array'))
-## for (bench in c('2-opt', 'Or-opt')) {
-##     pdf(paste0('matrix-', bench, '-boxplot.pdf'))
-##     title <- paste0('Relative CPU effort with matrix representation (', bench, ')')
-##     p <- ggplot(subset(ds, benchmark == bench),
-##                 aes(x=factor(n), y=normalised, fill=matrix)) +
-##         facet_wrap(~language, scales='free_y') +
-##         geom_boxplot() + labs(x='n',
-##                               y='CPU time (ratio of C++14 time)',
-##                               fill='Matrix representation',
-##                               title=title)
-##     print(p)
-##     dev.off()
-## }
 
 ## Impact of using static arrays
 ds <- subset(runs, implementation %in% c('c++14', 'c++14-static-arrays',
@@ -94,11 +85,13 @@ for (lang in c('C++14', 'Java')) {
         title <- paste0(lang, ': relative CPU effort of array type\n(',
                         bench, ')')
         p <- ggplot(subset(ds, language == lang & benchmark == bench),
-                    aes(x=factor(n), y=normalised, fill=implementation)) +
-            geom_boxplot() + labs(x='n',
-                                  y='CPU time (ratio of C++14 time)',
-                                  fill='Implementation',
-                                  title=title) + theme_bw()
+                    aes(y=normalised,
+                        color=implementation, fill=after_scale(color))) +
+            facet_wrap(~n, strip.position="bottom") + 
+            geom_boxplot(alpha=0.2, outlier.alpha=.5) +
+            labs(x='n', y='CPU time (ratio of C++14 time)',
+                 color='Implementation', title=title) +
+            theme_bw() + mytheme + palette
         print(p)
         dev.off()
     }
@@ -111,11 +104,13 @@ ds <- subset(runs, implementation %in% c('python', 'python-flat-matrix',
                                          'numpy', 'numpy-flat-matrix',
                                          'numba', 'numba-flat-matrix'))
 ds$impl <- as.character(ds$implementation)
-ds <- ds %>% mutate(interpreter = case_when(
-                        startsWith(impl, 'pypy') ~ 'Pypy',
-                        startsWith(impl, 'numba') ~ 'Numba',
-                        startsWith(impl, 'numpy') ~ 'Numpy',
-                        TRUE ~ 'CPython'))
+ds <- ds %>% mutate(interpreter = factor(
+                        case_when(
+                            startsWith(impl, 'pypy') ~ 'Pypy',
+                            startsWith(impl, 'numba') ~ 'Numba',
+                            startsWith(impl, 'numpy') ~ 'Numpy',
+                            TRUE ~ 'CPython'),
+                        levels=c('CPython','Pypy','Numpy','Numba')))
 
 for (bench in c('2-opt', 'Or-opt')) {
     for (inter in c('CPython', 'Pypy', 'Numba', 'Numpy')) {
@@ -123,11 +118,12 @@ for (bench in c('2-opt', 'Or-opt')) {
         title <- paste0(inter,
                         ': relative CPU effort of matrix representation (', bench, ')')
         p <- ggplot(subset(ds, interpreter == inter & benchmark == bench),
-                    aes(x=factor(n), y=normalised, fill=matrix)) +
-            geom_boxplot() + labs(x='n',
-                                  y='CPU time (ratio of C++14 time)',
-                                  fill='Matrix representation',
-                                  title=title) + theme_bw()
+                    aes(y=normalised, color=matrix, fill=after_scale(color))) +
+            facet_wrap(~n, strip.position="bottom") + 
+            geom_boxplot(alpha=0.2, outlier.alpha=.5) +
+            labs(x='n', y='CPU time (ratio of C++14 time)',
+                 color='Matrix\nrepresentation', title=title) +
+            theme_bw() + mytheme + palette
         print(p)
         dev.off()
     }
@@ -140,27 +136,29 @@ for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'maxflow')) {
     title <-paste0('Relative CPU effort of Python implementations (', bench,')')
     pdf(paste0('python_implementations-', bench, '-boxplot.pdf'))
     p <- ggplot(subset(pythonbest, benchmark == bench),
-                aes(x=factor(n), y=normalised,
-                    fill=factor(interpreter,
-                                levels=c('CPython','Pypy','Numpy','Numba')))) +
-        geom_boxplot() + labs(x='n',
-                              y='CPU time (ratio of C++14 time)',
-                              fill='Implementation',
-                              title=title) + theme_bw()
+                aes(y=normalised,
+                    color=interpreter, fill=after_scale(color))) +
+        facet_wrap(~n, strip.position="bottom") + 
+        geom_boxplot(alpha=0.2, outlier.alpha=.5) +
+        labs(x='n', y='CPU time (ratio of C++14 time)', title=title,
+             color='Implementation') +
+        theme_bw() + mytheme + palette
     print(p)
     dev.off()
 }
+
 for (bench in c('2-opt', 'Or-opt', 'maxflow')) {
     title <- paste0('Pypy vs Numba (', bench, ')')
     pdf(paste0('pypy_vs_numba-', bench, '-boxplot.pdf'))
     p <- ggplot(subset(pythonbest, benchmark == bench &
                        interpreter %in% c('Pypy', 'Numba')),
-                aes(x=factor(n), y=normalised,
-                    fill=factor(interpreter,))) +
-        geom_boxplot() + labs(x='n',
-                              y='CPU time (ratio of C++14 time)',
-                              fill='Implementation',
-                              title=title) + theme_bw()
+                aes(y=normalised,
+                    color=factor(interpreter,), fill=after_scale(color))) +
+        facet_wrap(~n, strip.position="bottom") + 
+        geom_boxplot(alpha=0.2, outlier.alpha=.5) +
+        labs(x='n', y='CPU time (ratio of C++14 time)',
+             color='Implementation', title=title) +
+        theme_bw() + mytheme + palette
     print(p)
     dev.off()
 }
@@ -171,11 +169,12 @@ for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'maxflow')) {
     pdf(paste0('C++14_vs_C++98-', bench, '-boxplot.pdf'))
     title <- paste0('C++14 vs C++98 (', bench, ')')
     p <- ggplot(subset(ds, benchmark == bench),
-                aes(x=factor(n), y=normalised, fill=language)) +
-        geom_boxplot() + labs(x='n',
-                              y='CPU time (ratio of C++14 time)',
-                              fill='Language',
-                              title=title) + theme_bw()
+                aes(y=normalised, color=language, fill=after_scale(color))) +
+        facet_wrap(~n, strip.position="bottom") + 
+        geom_boxplot(alpha=0.2, outlier.alpha=.5) +
+        labs(x='n', y='CPU time (ratio of C++14 time)',
+             color='Language', title=title) +
+        theme_bw() + mytheme + palette
     print(p)
     dev.off()
 }
@@ -188,11 +187,12 @@ for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'espprc-index', 'maxflow')){
     pdf(paste0('cross_language-', bench, '-boxplot.pdf'))
     title <- paste0('General cross-language comparison (', bench, ')')
     p <- ggplot(subset(ds, benchmark == bench),
-                aes(x=factor(n), y=normalised, fill=language)) +
-        geom_boxplot() + labs(x='n',
-                              y='CPU time (ratio of C++14 time)',
-                              fill='Language',
-                              title=title) + theme_bw()
+                aes(y=normalised, color=language, fill=after_scale(color))) +
+        facet_wrap(~n, strip.position="bottom") + 
+        geom_boxplot(alpha=0.2, outlier.alpha=.5) +
+        labs(x='n', y='CPU time (ratio of C++14 time)',
+             color='Language', title=title) + 
+        theme_bw() + mytheme + palette
     print(p)
     dev.off()
 }
@@ -204,11 +204,12 @@ for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'espprc-index', 'maxflow')){
     pdf(paste0('fast_languages-', bench, '-boxplot.pdf'))
     title <- paste0('Relative performance of "fast" languages (', bench, ')')
     p <- ggplot(subset(ds, benchmark == bench),
-                aes(x=factor(n), y=normalised, fill=language)) +
-        geom_boxplot() + labs(x='n',
-                              y='CPU time (ratio of C++14 time)',
-                              fill='Language',
-                              title=title) + theme_bw()
+                aes(y=normalised, color=language, fill=after_scale(color))) +
+        facet_wrap(~n, strip.position="bottom") + 
+        geom_boxplot(alpha=0.2, outlier.alpha=.5) +
+        labs(x='n', y='CPU time (ratio of C++14 time)',
+             color='Language', title=title) +
+        theme_bw() + mytheme + palette
     print(p)
     dev.off()
 }
@@ -221,14 +222,47 @@ for (bench in c('2-opt', 'Or-opt', 'lns', 'espprc', 'maxflow')){
     title <- paste0('Relative performance of "interpreted" languages (',
                     bench, ')')
     p <- ggplot(subset(ds, benchmark == bench),
-                aes(x=factor(n), y=normalised, fill=language)) +
-        geom_boxplot() + labs(x='n',
-                              y='CPU time (ratio of C++14 time)',
-                              fill='Language',
-                              title=title) + theme_bw()
+                aes(y=normalised, color=language, fill=after_scale(color))) +
+        facet_wrap(~n, strip.position="bottom") + 
+        geom_boxplot(alpha=0.2, outlier.alpha=.5) +
+        labs(x='n', y='CPU time (ratio of C++14 time)',
+             color='Language', title=title) + 
+        theme_bw() + mytheme + palette
     print(p)
     dev.off()
 }
 
-## Cross-platform comparison
-## not clear what to do here - maybe just mention cases where different platforms yield different results?
+
+
+
+
+
+
+## end of currently used plots
+
+## ## Cross-platform comparison
+## ## not clear what to do here - maybe just mention cases where different platforms yield different results?
+
+
+
+## ## FAILED EXPERIMENT might work well with less outliers but as things are now,
+## ##                   a bit hard to read
+## ## ## distance matrix representation comparison
+## ## ds <- subset(runs,
+## ##              implementation %in% c('c++14', 'c++14-nested-matrix',
+## ##                                    'java', 'java-nested-matrix',
+## ##                                    'javascript', 'javascript-nested-matrix',
+## ##                                    'julia-flat-matrix', 'julia-array'))
+## ## for (bench in c('2-opt', 'Or-opt')) {
+## ##     pdf(paste0('matrix-', bench, '-boxplot.pdf'))
+## ##     title <- paste0('Relative CPU effort with matrix representation (', bench, ')')
+## ##     p <- ggplot(subset(ds, benchmark == bench),
+## ##                 aes(x=factor(n), y=normalised, fill=matrix)) +
+## ##         facet_wrap(~language, scales='free_y') +
+## ##         geom_boxplot() + labs(x='n',
+## ##                               y='CPU time (ratio of C++14 time)',
+## ##                               fill='matrix representation',
+## ##                               title=title)
+## ##     print(p)
+## ##     dev.off()
+## ## }
